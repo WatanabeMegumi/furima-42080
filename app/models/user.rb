8de29133bf3042d6cda5cd586be4f9b19421380c
WatanabeMegumi <wmegumi001@gmail.com>
2025-03-06 12:51:28 +0900

    購入機能に必要なモデルとコントローラーの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :orders
 
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
