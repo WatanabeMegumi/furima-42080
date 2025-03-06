ae2ca7239a6bd0d98a31b8e9394d72002f3355fa
WatanabeMegumi <wmegumi001@gmail.com>
2025-03-06 21:23:45 +0900

    ビューの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,16 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
 
   def index
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-    @order_address = OrderAddress.new
+    if current_user == @item.user
+      redirect_to root_path
+    elsif @item.order.present?
+      redirect_to root_path
+    else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      @order_address = OrderAddress.new
+    end
   end
 
   def create
