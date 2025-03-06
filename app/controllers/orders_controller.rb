9cf22fa82f76cb949b8b21540cc87bb3870e49cb
WatanabeMegumi <wmegumi001@gmail.com>
2025-03-06 16:02:26 +0900

    購入のためのフォーム実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,17 +6,18 @@ class OrdersController < ApplicationController
 
   def create
     binding.pry
-    @order_address = OrderAddress.new(order_params)
+    @order_address = OrderAddress.new #(order_params)
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
     else
       render :index, status: :unprocessable_entity
     end
+  end
 
     private
 
-    def order_params
-      params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: #)
-    end
+    #def order_params
+      #params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item_id)
+    #end
 end
